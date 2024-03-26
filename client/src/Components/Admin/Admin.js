50b5707b77bea50f21283cb61d018e9a4861e3a3
Jacob G <jrgrant560@gmail.com>
2024-03-26 17:38:03 -0400

    updated storyguide; added basic Login View operations

--- client/src/Components/Admin/Admin.js
+++ client/src/Components/Admin/Admin.js
@@ -24,78 +24,84 @@ const Admin = () => {
     const buttonExport = AdminStyle.buttonExport;
     const table = AdminStyle.table;
 
+    let adminIsLoggedIn = false;
+
 
     return (
         <main className={mainContainer}>
+            {!adminIsLoggedIn ? <section>
+                Login View
+            </section> :
+                <section>
+                    <div className={header}>
+                        <div className={headerEmpty}></div>
+                        <h1 className={headerTitle}>Welcome, Brandis</h1>
+                        <Button className={buttonLogout}>Log Out</Button>
+                    </div>
 
-            <div className={header}>
-                <div className={headerEmpty}></div>
-                <h1 className={headerTitle}>Welcome, Brandis</h1>
-                <Button className={buttonLogout}>Log Out</Button>
-            </div>
-
-            <Form
-            id="formSearch"
-                // ref={refForm}
-                onSubmit={() => {}}
-                // validated={validated}
-                className={formSearch}
-                autoComplete="on"
-            >
-                <Input className={formSearchBar} placeholder="Search" inputProps={{ 'aria-label': 'description' }} />
-                <Button className={buttonSearch} type="submit">Search</Button>
-            </Form>
-            <Button className={buttonExport}>Export this List</Button>
-            <Table className={table}>
-                <Thead>
-                    <Tr>
-                        <Th>Event</Th>
-                        <Th>Ticket</Th>
-                        <Th>Amount Paid</Th>
-                        <Th>First Name</Th>
-                        <Th>Last Name</Th>
-                        <Th>Title</Th>
-                        <Th>Practice</Th>
-                        <Th>Email</Th>
-                        <Th>Phone #</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    <Tr>
-                        <Td>Lorem Ipsum</Td>
-                        <Td>123456789</Td>
-                        <Td>$1234.56</Td>
-                        <Td>John</Td>
-                        <Td>Doe</Td>
-                        <Td>Doctor</Td>
-                        <Td>John's Clinic</Td>
-                        <Td>johndoe@example.com</Td>
-                        <Td>123-456-7890</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>Lorem Ipsum</Td>
-                        <Td>123456789</Td>
-                        <Td>$1234.56</Td>
-                        <Td>John</Td>
-                        <Td>Doe</Td>
-                        <Td>Doctor</Td>
-                        <Td>John's Clinic</Td>
-                        <Td>johndoe@example.com</Td>
-                        <Td>123-456-7890</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>Lorem Ipsum</Td>
-                        <Td>123456789</Td>
-                        <Td>$1234.56</Td>
-                        <Td>John</Td>
-                        <Td>Doe</Td>
-                        <Td>Doctor</Td>
-                        <Td>John's Clinic</Td>
-                        <Td>johndoe@example.com</Td>
-                        <Td>123-456-7890</Td>
-                    </Tr>
-                </Tbody>
-            </Table>
+                    <Form
+                        id="formSearch"
+                        // ref={refForm}
+                        onSubmit={() => { }}
+                        // validated={validated}
+                        className={formSearch}
+                        autoComplete="on"
+                    >
+                        <Input className={formSearchBar} placeholder="Search" inputProps={{ 'aria-label': 'description' }} />
+                        <Button className={buttonSearch} type="submit">Search</Button>
+                    </Form>
+                    <Button className={buttonExport}>Export this List</Button>
+                    <Table className={table}>
+                        <Thead>
+                            <Tr>
+                                <Th>Event</Th>
+                                <Th>Ticket</Th>
+                                <Th>Amount Paid</Th>
+                                <Th>First Name</Th>
+                                <Th>Last Name</Th>
+                                <Th>Title</Th>
+                                <Th>Practice</Th>
+                                <Th>Email</Th>
+                                <Th>Phone #</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td>Lorem Ipsum</Td>
+                                <Td>123456789</Td>
+                                <Td>$1234.56</Td>
+                                <Td>John</Td>
+                                <Td>Doe</Td>
+                                <Td>Doctor</Td>
+                                <Td>John's Clinic</Td>
+                                <Td>johndoe@example.com</Td>
+                                <Td>123-456-7890</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>Lorem Ipsum</Td>
+                                <Td>123456789</Td>
+                                <Td>$1234.56</Td>
+                                <Td>John</Td>
+                                <Td>Doe</Td>
+                                <Td>Doctor</Td>
+                                <Td>John's Clinic</Td>
+                                <Td>johndoe@example.com</Td>
+                                <Td>123-456-7890</Td>
+                            </Tr>
+                            <Tr>
+                                <Td>Lorem Ipsum</Td>
+                                <Td>123456789</Td>
+                                <Td>$1234.56</Td>
+                                <Td>John</Td>
+                                <Td>Doe</Td>
+                                <Td>Doctor</Td>
+                                <Td>John's Clinic</Td>
+                                <Td>johndoe@example.com</Td>
+                                <Td>123-456-7890</Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                </section>}
         </main>
     );
 };
---
+++ client/src/Components/Admin/STORYGUIDE.md
@@ -0,0 +1,17 @@
+- [x] As An ADMIN, I want a hidden Page to view my internal operations
+	- [x] As a DEV, I want...
+		- [x] an admin page
+		
+- [x] As An ADMIN, I want my admin page to have user authentication
+    - [ ] As a DEV, I want...
+        - [ ] a ternary view for login/logout only; this will swap with the main view depending on login status
+
+- [x] As an ADMIN, I want to see registrants' Event Name, Ticket #, Amount Paid, First Name, Last Name, Professional Title/Position, Email, Phone Number, Practice Name, Practice Address (street #, city, state, country, postal code), etc.
+	- [x] As a DEV, I want...
+		- [x] this page to display a table
+			- [x] this table to display a row for every ticket purchased
+
+
+BACKLOG FEATURES
+- [x] As an ADMIN, I want a search field that filters registrants by ???
+- [ ] As an Admin, I want a dropdown field to filter by events
