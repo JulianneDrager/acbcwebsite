21915f7739156a440e67bb10994556a81ab8c382
Julianne35 <jbosko28@gmail.com>
2024-03-28 02:34:06 -0400

    fixed url in oAuth

--- server/api/OAuth.js
+++ server/api/OAuth.js
@@ -14,46 +14,44 @@ const clientSecretENV = process.env.SQUARE_CLIENT_SECRET;
 router.get("/authorize", (req, res) => {
   const clientId = clientIDENV;
 
-  const redirectUri = "https://acbcwebsite.onrender.com/oauth/callback"; // Replace with your redirect URI.
-  const scopes = ["CUSTOMERS_READ", "CUSTOMERS_WRITE"]; // Replace with the scopes your application needs
+  const redirectUri = "https://acbcwebsite.onrender.com/oauth/callback";
 
-  const authUrl = `https://connect.squareup.com/oauth2/authorize?client_id=${clientId}&response_type=code&scope=${scopes.join(
-    " "
-  )}&redirect_uri=${redirectUri}`;
+  const authUrl = `https://connect.squareup.com/oauth2/authorize?client_id=${clientIDENV}=CUSTOMERS_READ CUSTOMERS_WRITE&session=false&state=NY&redirect_uri=${redirectUri}`;
 
   res.redirect(authUrl);
 });
 
 // Callback route for Square to redirect to
-router.get("oauth/callback", async (req, res) => {
+router.get("/callback", async (req, res) => {
+  console.log("Callback route hit");
+
   const { code } = req.query;
 
   // Log the entire query parameters and the authorization code
   console.log("Query parameters: ", req.query);
   console.log("Authorization code: ", code);
-});
 
-// Callback route for Square to redirect to
-router.get("/callback", async (req, res) => {
-  const { code } = req.query;
+  try {
+    const response = await axios.post(
+      "https://connect.squareup.com/oauth2/token",
+      qs.stringify({
+        client_id: clientIDENV,
+        client_secret: clientSecretENV,
+        code,
+        grant_type: "authorization_code",
+      })
+    );
 
-  const response = await axios.post(
-    "https://connect.squareup.com/oauth2/token",
-    qs.stringify({
-      client_id: clientIDENV,
-      client_secret: clientSecretENV,
-      code,
-      grant_type: "authorization_code",
-    })
-  );
+    const { access_token } = response.data;
 
-  const { access_token } = response.data;
+    // Store the access token and use it to authenticate your API calls
+    // This is a simple example and does not cover token storage or refresh
+    // You'll need to implement token storage and refresh according to your application's needs
 
-  // Store the access token and use it to authenticate your API calls
-  // This is a simple example and does not cover token storage or refresh
-  // You'll need to implement token storage and refresh according to your application's needs
-
-  console.log("Access token: ", access_token);
+    console.log("Access token: ", access_token);
+  } catch (error) {
+    console.error("Error when making request to Square: ", error);
+  }
 });
 
 module.exports = router;
