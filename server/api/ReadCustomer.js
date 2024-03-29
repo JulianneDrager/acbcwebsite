f215dd70416568b4a457c060ea6256e4c7a5889b
Julianne35 <jbosko28@gmail.com>
2024-03-29 00:33:37 -0400

    added consle

--- server/api/ReadCustomer.js
+++ server/api/ReadCustomer.js
@@ -31,7 +31,8 @@ async function listCustomers() {
     const response = await squareClient.customersApi.listCustomers();
     console.log("Response from listCustomers API:", response);
     const customers = response.result.customers;
-    console.log("customers", customers);
+    // console.log("customers", customers);
+    console.log("Result from listCustomers API:", response.result);
     return customers;
   } catch (error) {
     console.error("Error listing customers:", error);
