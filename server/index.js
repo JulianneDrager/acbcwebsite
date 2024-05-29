1734acf768bae22e8c53c9d9d139a35947e305bf
Julianne Drager <67062523+JulianneDrager@users.noreply.github.com>
2024-05-29 16:37:39 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -122,7 +122,7 @@ app.post("/send-email", async (req, res) => {
     );
 
     const registerEmailHtml = render(
-      React.createElement(RegisterEmail, {
+      React.createElement(RegisterEmailResponse, {
         firstName: firstName,
         lastName: lastName,
         email: email,
