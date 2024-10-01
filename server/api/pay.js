23197e3b49e08bb169b2f66712a5f1a56b706790
Julianne Drager <67062523+JulianneDrager@users.noreply.github.com>
2024-10-01 19:06:02 -0400

    Add files via upload

--- server/api/pay.js
+++ server/api/pay.js
@@ -27,7 +27,7 @@ const handlePayment = async (req, res) => {
         idempotencyKey: uuidv4(),
         sourceId: req.body.sourceId,
         amountMoney: {
-          amount: 100, // Removed duplicate key
+          amount: 0.01, // Removed duplicate key
           currency: "USD",
         },
       });
@@ -45,3 +45,43 @@ const handlePayment = async (req, res) => {
 };
 
 exports.handlePayment = handlePayment;
+
+// require("dotenv").config();
+// const { Client, Environment } = require("square");
+// const { v4: uuidv4 } = require("uuid");
+
+// BigInt.prototype.toJSON = function () {
+//   return this.toString();
+// };
+
+// const client = new Client({
+//   accessToken: process.env.SQUARE_ACCESS_TOKEN,
+//   environment: Environment.Production,
+// });
+
+// const handlePayment = async (req, res) => {
+//   console.log("Received request");
+//   if (req.method === "POST") {
+//     const { sourceId } = req.body;
+//     console.log("Request body:", req.body);
+//     try {
+//       const response = await client.paymentsApi.createPayment({
+//         idempotencyKey: uuidv4(),
+//         sourceId,
+//         amountMoney: {
+//           amount: 100, // Amount in cents
+//           currency: "USD",
+//         },
+//       });
+//       console.log("Payment response:", response);
+//       res.status(200).json(response.result);
+//     } catch (error) {
+//       console.error("Error creating payment:", error);
+//       res.status(500).json({ error: error.message });
+//     }
+//   } else {
+//     res.status(405).json({ message: "Method not allowed" });
+//   }
+// };
+
+// module.exports = { handlePayment };
