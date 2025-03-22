d6bc9736f72b94b5fca5b68eb9356434beed2a4c
Julianne Drager <67062523+JulianneDrager@users.noreply.github.com>
2025-03-22 09:00:01 -0400

    Add files via upload

--- server/routes/EventRouter.js
+++ server/routes/EventRouter.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const RegisterController = require("../controllers/RegisterController");
+const RegisterIndController = require("../controllers/RegisterIndController");
 
 // Routes for Registers
 router.get("/get", RegisterController.getAllRegisters);
@@ -9,5 +10,12 @@ router.post("/create", RegisterController.createRegister);
 router.put("/update/:id", RegisterController.updateRegister);
 
 router.delete("/delete/:id", RegisterController.deleteRegister);
+
+router.get("/get-ind", RegisterIndController.getAllRegisters);
+router.get("/get-ind/:id", RegisterIndController.getRegisterById);
+router.post("/create-ind", RegisterIndController.createRegister);
+router.put("/update-ind/:id", RegisterIndController.updateRegister);
+
+router.delete("/delete-ind/:id", RegisterIndController.deleteRegister);
 
 module.exports = router;
--- server/routes/ExtractEmailsRouter.js
+++ server/routes/ExtractEmailsRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const extractEmailsController = require("../controllers/ExtractEmailsController");
+const extractEmailsIndController = require("../controllers/ExtractEmailsIndController");
 
 // Routes for Registers
 router.get("/extract-emails", extractEmailsController.extractEmails);
+router.get("/extract-ind-emails", extractEmailsIndController.extractIndEmails);
 
 module.exports = router;
